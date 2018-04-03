15082e92b2699b3dba2098d3b69fa0a38a2b9b64
kevinykuo <kevinykuo@gmail.com>
2018-04-03 17:38:08 -0400

    add example to ml_write_bundle()

--- R/mleap.R
+++ R/mleap.R
@@ -88,6 +88,24 @@ mleap_load_bundle <- function(path) {
 #' @param dataset A Spark DataFrame with the schema of the transformed DataFrame.
 #' @param path Where to save the bundle.
 #' @param overwrite Whether to overwrite an existing file, defaults to \code{FALSE}.
+#' 
+#' @examples
+#' \dontrun{
+#' library(sparklyr)
+#' sc <- spark_connect(master = "local")
+#' mtcars_tbl <- sdf_copy_to(sc, mtcars, overwrite = TRUE)
+#' pipeline <- ml_pipeline(sc) %>%
+#'   ft_binarizer("hp", "big_hp", threshold = 100) %>%
+#'   ft_vector_assembler(c("big_hp", "wt", "qsec"), "features") %>%
+#'   ml_gbt_regressor(label_col = "mpg")
+#' pipeline_model <- ml_fit(pipeline, mtcars_tbl)
+#' model_path <- file.path(tempdir(), "mtcars_model.zip")
+#' ml_write_bundle(pipeline_model, 
+#'                 ml_transform(pipeline_model, mtcars_tbl),
+#'                 model_path,
+#'                 overwrite = TRUE)
+#' }
+#' 
 #' @export
 ml_write_bundle <- function(x, dataset, path, overwrite = FALSE) {
   stages <- if (purrr::is_bare_list(x)) {
