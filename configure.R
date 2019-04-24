cce1852039f54ed422c31e910518dc4f20ecc2b1
lgong <lgong@microsoft.com>
2019-04-24 10:52:26 -0700

    create folder "internal/mleap-spark" if it does not exist already.

--- configure.R
+++ configure.R
@@ -1,14 +1,20 @@
 #!/usr/bin/env Rscript
 library(purrr)
+
+path <- "internal/mleap-spark"
+if (!dir.exists(path)) {
+  dir.create(path, recursive = TRUE)
+}
+
 mleap:::download_jars(mleap:::resolve_maven_path(),
                       "ml.combust.mleap:mleap-spark_2.11:0.10.1",
-                      normalizePath("internal/mleap-spark"),
+                      normalizePath(path),
                       use_temp_cache = TRUE
                       )
 
 spec <- sparklyr::spark_default_compilation_spec() %>%
   map(function(x) {
-    x$jar_dep <- list.files("internal/mleap-spark", full.names = TRUE) %>% 
+    x$jar_dep <- list.files(path, full.names = TRUE) %>% 
       map_chr(normalizePath)
     x
   }) %>%
