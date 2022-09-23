cfa403529bb3a7de69d88aa600f1ec8665ecd000
Edgar Ruiz <edgararuiz@gmail.com>
2022-09-23 08:14:04 -0500

    Adds ability to download jars sets for different mleap and scala versions

--- utils/configure.R
+++ utils/configure.R
@@ -4,71 +4,89 @@ library(sparklyr)
 devtools::load_all()
 
 
-mleap_path <- path("internal/mleap-spark/")
-if(!dir_exists(mleap_path)) {
-  dir_create(mleap_path)
-  download_jars(
-    resolve_maven_path(),
-    "ml.combust.mleap:mleap-spark_2.12:0.20.0",
-    mleap_path,
-    use_temp_cache = TRUE
+versions <- list(
+  list(scala = "2.12", mleap = "0.20.0"),
+  list(scala = "2.11", mleap = "0.16.0")
+)
+
+base_folder <- "internal/mleap-spark"
+url_maven <- "https://repo1.maven.org/maven2"
+
+download_jars <- function(scala_version, mleap_version, base_folder, url_maven) {
+  jar_path <- path(base_folder, scala_version)
+  
+  if(!dir_exists(jar_path)) dir_create(jar_path)
+  
+  
+  url_combust <- "ml/combust/"
+  
+  url_mleap_pkgs <- c("avro", "base", "executor-testkit", "executor", 
+                      "runtime", "serving", "spark-base", "spark-extension",
+                      "spark-testkit", "spark", "core"
+  )
+  
+  url_mleap_vers <- paste0("mleap-", url_mleap_pkgs, "_", scala_version)
+  
+  url_mleap <- paste0(
+    url_maven, "/",
+    url_combust, 
+    "mleap", "/", 
+    url_mleap_vers, "/", 
+    mleap_version, "/",
+    url_mleap_vers, "-",
+    mleap_version, ".jar"
+  )
+  
+  url_arm <- paste0(url_maven, "/com/jsuereth/scala-arm_", 
+                    scala_version,"/2.0/scala-arm_", 
+                    scala_version,"-2.0.jar"
+  )
+  
+  url_bundle <- paste0(url_maven, "/ml/combust/bundle/bundle-ml_", 
+                       scala_version,"/", mleap_version,"/bundle-ml_", 
+                       scala_version, "-",  mleap_version,".jar"
+  )
+  
+  url_all <- c(url_mleap, url_arm, url_bundle)
+  
+  walk(
+    url_all,
+    ~ {
+      jar_target <- path(jar_path, path_file(.x))
+      if(!file_exists(jar_target)) download.file(.x, jar_target)
+    }
   )
 }
 
-scala_version <- "2.12"
-mleap_version <- "0.20.0"
-
-url_combust <- "https://repo1.maven.org/maven2/ml/combust/"
-
-url_mleap_pkgs <- c("avro", "base", "executor-testkit", "executor", 
-                    "runtime", "serving", "spark-base", "spark-extension",
-                    "spark-testkit", "spark", "core"
-)
-
-url_mleap_vers <- paste0("mleap-", url_mleap_pkgs, "_", scala_version)
-
-url_mleap <- paste0(
-  url_combust, 
-  "mleap", "/", 
-  url_mleap_vers, "/", 
-  mleap_version, "/",
-  url_mleap_vers, "-",
-  mleap_version, ".jar"
-  )
-
-map(
-  url_mleap,
-  ~ download.file(.x, path("internal/mleap-spark", path_file(.x)))
-)
-
-download.file(
-  "https://repo1.maven.org/maven2/com/jsuereth/scala-arm_2.12/2.0/scala-arm_2.12-2.0.jar",
-  "internal/mleap-spark/scala-arm_2.12-2.0.jar"
-)
-
-
-download.file(
-  "https://repo1.maven.org/maven2/ml/combust/bundle/bundle-ml_2.12/0.20.0/bundle-ml_2.12-0.20.0.jar",
-  "internal/mleap-spark/bundle-ml_2.12-0.20.0.jar"
+walk(
+  versions,
+  ~ download_jars(.x$scala, .x$mleap, base_folder, url_maven)
 )
 
 
 sparklyr_spec <- spark_default_compilation_spec()
 
-mleap_files <- mleap_path %>% 
-  dir_ls() %>% 
+mleap_files <- base_folder %>% 
+  dir_ls(recurse = TRUE) %>% 
   path_abs()
 
+scala_paths <- map(
+  versions, ~ c(.x, list(path = find_scalac(.x$scala), 
+                         jars = path_abs(dir_ls(path(base_folder, .x$scala)))
+                         )))
+
 mleap_spec <- sparklyr_spec %>% 
+  keep(~ .x$spark_version >= "2.4.0")  %>% 
   map(~{
     x <- .x
-    x$jar_dep <- mleap_files
-    #x$jar_dep <- "ml.combust.mleap:mleap-spark_2.12:0.20.0"
+    mt <- scala_paths[map_lgl(scala_paths, ~ x$scalac_path == .x$path)]
+    x$jar_dep <- mt[[1]]$jars
     x$embedded_srcs <- NULL
     x
-  }) %>% 
-  keep(~ .x$spark_version >= "2.4.0")  
+  }) 
 
-compile_package_jars(spec = mleap_spec[[2]])
+compile_package_jars(spec = mleap_spec)
+
+
 
 
