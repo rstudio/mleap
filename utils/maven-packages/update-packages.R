3444dd8c21c643d48dacc8d0df1efbba013744f7
Edgar Ruiz <edgararuiz@gmail.com>
2022-10-14 09:16:19 -0500

    Finalizes MLeap Maven links parser, adds pkg rds

--- utils/maven-packages/update-packages.R
+++ utils/maven-packages/update-packages.R
@@ -3,17 +3,17 @@ library(rvest)
 library(purrr)
 library(stringr)
 library(dplyr)
+library(readr)
 
 devtools::load_all()
 
 maven_repo <- mleap_get_session_defaults("installation", "maven", "repo")
 
+combust_root <- c("ml", "combust")
 
+package_roots <- c("bundle", "mleap")
 
-mleap_root <- c("ml", "combust", "mleap")
-mleap_url <- paste0(maven_repo, paste0(mleap_root, collapse = "/"))
-
-
+base_urls <- paste0(maven_repo, paste0(combust_root, collapse = "/"), "/", package_roots)
 
 get_links <- function(x)  {
   links <- curl(x) %>%
@@ -22,31 +22,40 @@ get_links <- function(x)  {
     html_attr("href")
   ret <- links[links != "../"]
   ret <- ret[!str_detect(ret, "maven-metadata")]
-  
+  ret
 }
 
-mleap_links <- get_links(mleap_url)
-mleap_links <- mleap_links %>% 
-  imap(~ get_links(paste0(mleap_url, "/", .x))) %>% 
-  set_names(mleap_links)
+base_links <- base_urls %>% 
+  map(~ paste(.x, get_links(.x), sep = "/")) %>% 
+  flatten() 
 
+all_links <- base_links %>% 
+  map(get_links) %>% 
+  set_names(base_links)
 
-maven_root <- paste0(mleap_root, collapse = ".")
-
-mleap_entries <- mleap_links %>% 
+mleap_packages <- all_links %>% 
   imap_dfr(~ {
-    pkg_name <- str_remove(.y, "/")
+    base_name <- .y
+    for(i in seq_along(base_urls)) {
+      base_name <- str_remove(base_name, base_urls[[i]])
+    }
+    pkg_name <- str_remove_all(base_name, "/")
     name_split <- str_split(pkg_name, "_")[[1]]
+    maven <- .y %>% 
+      str_remove(maven_repo) %>% 
+      str_remove(base_name) %>% 
+      str_replace_all("/", ".")
+    
     mleap <- str_remove(.x, "/")
     tibble(
       mleap = mleap,
       scala = name_split[[2]], 
-      name = str_remove(name_split[[1]], "mleap-"), 
-      package = paste(maven_root, pkg_name, mleap, sep = ":")
+      name = str_remove(name_split[[1]], "mleap-"),
+      package = paste(maven, pkg_name, mleap, sep = ":")
     )
   }) 
 
-mleap_versions <- mleap_entries %>% 
+mleap_versions <- mleap_packages %>% 
   group_by(mleap, scala) %>% 
   summarise() %>% 
   group_by(mleap) %>% 
@@ -55,5 +64,6 @@ mleap_versions <- mleap_entries %>%
 
 mleap_defaults <- mleap_versions[mleap_versions$default == "yes", ]
 
-mleap_defaults[mleap_defaults$mleap == "0.10.0", ]
-
+write_rds(mleap_packages, "inst/extdata/mleap_packages.rds")
+write_rds(mleap_versions, "inst/extdata/mleap_versions.rds")
+write_rds(mleap_defaults, "inst/extdata/mleap_defaults.rds")
